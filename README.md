828167b257bd66f45ce2fe61890cd5f25e9ebbd4
Vinsensius Andre Wijaya <76654693+Andrematomer@users.noreply.github.com>
2026-06-15 14:41:06 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+This repo made for testing purposes. You're welcome to look around, but there's nothing interesting or secret here.
+
+Link to Github Pages of this repo: [text](https://andrematomer.github.io/Test/)
---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'talking-tan-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('SW: Pre-caching assets');
+        return cache.addAll(ASSETS);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('SW: Clearing old cache');
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+  );
+});
